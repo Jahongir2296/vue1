e26948d5cb64b7b0e4737c2a6d13ec72c476a25d
Jahongir <jaxa2296@mail.com>
2022-01-13 20:06:44 +0500

    vue loyiha

---
+++ app/src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Counter {{counter}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed:{
+            counter(){
+                return this.$store.getters.computedCounter
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
